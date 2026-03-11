ae7aa20e87f418bab9599ee932fe52941c76471e
TizianoBR <tizianobiondo@hotmail.com>
2026-03-11 10:43:00 +0100

    Fix generate-problem.py

--- Parte1/generate-problem.py
+++ Parte1/generate-problem.py
@@ -5,7 +5,7 @@
 # Based on the Linköping University TDDD48 2021 course.
 # https://www.ida.liu.se/~TDDD48/labs/2021/lab1/index.en.shtml
 #
-# Adapted for the emergency-logistics domain.
+# You mainly have to change the parts marked as TODO.
 #
 ########################################################################################
 
@@ -19,6 +19,9 @@ import sys
 # Hard-coded options
 ########################################################################################
 
+# Crates will have different contents, such as food and medicine.
+# You can change this to generate other contents if you want.
+
 content_types = ["food", "medicine"]
 
 
@@ -26,12 +29,21 @@ content_types = ["food", "medicine"]
 # Random seed
 ########################################################################################
 
+# Set seed to 0 if you want more predictability...
 # random.seed(0);
 
 ########################################################################################
 # Helper functions
 ########################################################################################
 
+# We associate each location with x/y coordinates.  These coordinates
+# won't actually be used explicitly in the domain, but we *will*
+# eventually use them implicitly by using *distances* in order to
+# calculate flight times.
+#
+# This function returns the euclidean distance between two locations.
+# The locations are given via their integer index.  You won't have to
+# use this other than indirectly through the flight cost function.
 def distance(location_coords, location_num1, location_num2):
     x1 = location_coords[location_num1][0]
     y1 = location_coords[location_num1][1]
@@ -40,9 +52,20 @@ def distance(location_coords, location_num1, location_num2):
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 
+# This function returns the action cost of flying between two
+# locations supplied by their integer indexes.  You can use this
+# function when you extend the problem generator to generate action
+# costs.
 def flight_cost(location_coords, location_num1, location_num2):
     return int(distance(location_coords, location_num1, location_num2)) + 1
 
+
+# When you run this script you specify the *total* number of crates
+# you want.  The function below determines randomly which crates
+# will have a specific type of contents.  crates_with_contents[0]
+# is a list of crates containing content_types[0] (in our
+# example "food"), and so on.
+# Note: Will have at least one crate per type!
 
 def setup_content_types(options):
     while True:
@@ -51,14 +74,22 @@ def setup_content_types(options):
         for x in range(len(content_types) - 1):
             types_after_this = len(content_types) - x - 1
             max_now = crates_left - types_after_this
+            # print x, types_after_this, crates_left, len(content_types), max_now
             num = random.randint(1, max_now)
+            # print num
             num_crates_with_contents.append(num)
             crates_left -= num
         num_crates_with_contents.append(crates_left)
+        # print(num_crates_with_contents)
 
+        # If we have 10 medicine and 4 food, with 7 people,
+        # we can support at most 7+4=11 goals.
         maxgoals = sum(min(num_crates, options.persons) for num_crates in num_crates_with_contents)
 
+        # Check if the randomization supports the number of goals we want to generate.
+        # Otherwise, try to randomize again.
         if options.goals <= maxgoals:
+            # Done
             break
 
     print()
@@ -79,6 +110,9 @@ def setup_content_types(options):
     return crates_with_contents
 
 
+# This function populates the location_coords list with an X and Y
+# coordinate for each location.  You won't have to use this other than
+# indirectly through the flight cost function.
 def setup_location_coords(options):
     location_coords = [(0, 0)]  # For the depot
     for x in range(1, options.locations + 1):
@@ -88,15 +122,22 @@ def setup_location_coords(options):
     return location_coords
 
 
+# This function generates a random set of goals.
+# After you run this, need[personid][contentid] is true if and only if
+# the goal is for the person to have a crate with the specified content.
+# You will use this to create goal statements in PDDL.
 def setup_person_needs(options, crates_with_contents):
     need = [[False for i in range(len(content_types))] for j in range(options.persons)]
     goals_per_contents = [0 for i in range(len(content_types))]
 
     for goalnum in range(options.goals):
+
         generated = False
         while not generated:
             rand_person = random.randint(0, options.persons - 1)
             rand_content = random.randint(0, len(content_types) - 1)
+            # If we have enough crates with that content
+            # and the person doesn't already need that content
             if (goals_per_contents[rand_content] < len(crates_with_contents[rand_content])
                     and not need[rand_person][rand_content]):
                 need[rand_person][rand_content] = True
@@ -110,6 +151,8 @@ def setup_person_needs(options, crates_with_contents):
 ########################################################################################
 
 def main():
+    # Take in all arguments and print them to standard output
+
     parser = OptionParser(usage='python generator.py [-help] options...')
     parser.add_option('-d', '--drones', metavar='NUM', dest='drones', action='store', type=int, help='the number of drones')
     parser.add_option('-r', '--carriers', metavar='NUM', type=int, dest='carriers',
@@ -126,27 +169,35 @@ def main():
     if options.drones is None:
         print("You must specify --drones (use --help for help)")
         sys.exit(1)
+
     if options.carriers is None:
         print("You must specify --carriers (use --help for help)")
         sys.exit(1)
+
     if options.locations is None:
         print("You must specify --locations (use --help for help)")
         sys.exit(1)
+
     if options.persons is None:
         print("You must specify --persons (use --help for help)")
         sys.exit(1)
+
     if options.crates is None:
         print("You must specify --crates (use --help for help)")
         sys.exit(1)
+
     if options.goals is None:
         print("You must specify --goals (use --help for help)")
         sys.exit(1)
+
     if options.goals > options.crates:
         print("Cannot have more goals than crates")
         sys.exit(1)
+
     if len(content_types) > options.crates:
         print("Cannot have more content types than crates:", content_types)
         sys.exit(1)
+
     if options.goals > len(content_types) * options.persons:
         print("For", options.persons, "persons, you can have at most", len(content_types) * options.persons, "goals")
         sys.exit(1)
@@ -159,6 +210,9 @@ def main():
     print("Goals\t\t", options.goals)
 
     # Setup all lists of objects
+
+    # These lists contain the names of all Drones, locations, and so on.
+
     drone = []
     person = []
     crate = []
@@ -170,30 +224,48 @@ def main():
         location.append("loc" + str(x + 1))
     for x in range(options.drones):
         drone.append("drone" + str(x + 1))
-    for x in range(options.carriers):
-        carrier.append("carrier" + str(x + 1))
+    #for x in range(options.carriers):
+        #carrier.append("carrier" + str(x + 1))
     for x in range(options.persons):
         person.append("person" + str(x + 1))
     for x in range(options.crates):
         crate.append("crate" + str(x + 1))
-
+    
+    # Determine the set of crates for each content.
+    # If content_types[0] is "food",
+    # then crates_with_contents[0] is a list
+    # containing the names of all crates that contain food.
     crates_with_contents = setup_content_types(options)
+
+    # Generates coordinates for each location.
+    # You will only use this indirectly,
+    # through the flight_cost() function in lab 2.
     location_coords = setup_location_coords(options)
+
+    # Determine which types of content each person needs.
+    # If person[0] is "person0",
+    # and content_types[1] is "medicine",
+    # then needs[0][1] is true iff person0 needs medicine.
     need = setup_person_needs(options, crates_with_contents)
 
+    # Define a problem name
     problem_name = "drone_problem_d" + str(options.drones) + "_r" + str(options.carriers) + \
                    "_l" + str(options.locations) + "_p" + str(options.persons) + "_c" + str(options.crates) + \
                    "_g" + str(options.goals) + "_ct" + str(len(content_types))
 
     # Open output file
     with open(problem_name + ".pddl", 'w') as f:
+        # Write the initial part of the problem
+
         f.write("(define (problem " + problem_name + ")\n")
-        f.write("(:domain emergency-logistics)\n")
+        f.write("(:domain drone-domain)\n")
         f.write("(:objects\n")
 
         ######################################################################
         # Write objects
-        # Types match the domain: drone, location, crate, content, person
+
+        # TODO: Change the type names below (drone, location, ...)
+        # to suit your domain.
 
         for x in drone:
             f.write("\t" + x + " - drone\n")
@@ -205,12 +277,13 @@ def main():
             f.write("\t" + x + " - crate\n")
 
         for x in content_types:
-            f.write("\t" + x + " - content\n")
+            f.write("\t" + x + " - contents\n")
 
         for x in person:
             f.write("\t" + x + " - person\n")
 
-        # Carriers omitted when count is 0 (no carrier type in this domain)
+        for x in carrier:
+            f.write("\t" + x + " - carrier\n")
 
         f.write(")\n")
 
@@ -219,25 +292,27 @@ def main():
 
         f.write("(:init\n")
 
-        # --- Drones: start at the depot, both arms free ---
-        for d in drone:
-            f.write("\t(at-drone " + d + " depot)\n")
-            f.write("\t(arm-free-left " + d + ")\n")
-            f.write("\t(arm-free-right " + d + ")\n")
+        # TODO: Initialize all facts here!
+        for x in drone:
+            f.write("\t(at-drone " + x + " depot)\n")
+            f.write("\t(arm-free-left " + x + ")\n")
+            f.write("\t(arm-free-right " + x + ")\n")
+        loclist = location.copy()
+        loclist.remove("depot")
+        for x in person:
+            f.write("\t(at-person " + x + " " + random.choice(loclist) + ")\n")
+        #for x in crate:
+        #    f.write("\t(at-crate " + x + " depot)\n")
+        #    f.write("\t(crate-content " + x + " " + random.choice(content_types) + ")\n")
+        crate_num = 0
+        for x in range(options.persons):
+            for y in range(len(content_types)):
+                if need[x][y]:
+                    person_name = person[x]
+                    content_name = content_types[y]
+                    f.write("\t(at-crate crate" + str(crate_num + 1) + " depot)\n")
+                    f.write("\t(crate-content crate" + str(crate_num + 1) + " " + content_name + ")\n")
 
-        # --- Persons: each at a random non-depot location ---
-        for p in person:
-            rand_loc = location[random.randint(1, len(location) - 1)]
-            f.write("\t(at-person " + p + " " + rand_loc + ")\n")
-
-        # --- Crates: all start at the depot ---
-        for c in crate:
-            f.write("\t(at-crate " + c + " depot)\n")
-
-        # --- Content of each crate ---
-        for content_idx in range(len(content_types)):
-            for c in crates_with_contents[content_idx]:
-                f.write("\t(crate-content " + c + " " + content_types[content_idx] + ")\n")
 
         f.write(")\n")
 
@@ -246,16 +321,19 @@ def main():
 
         f.write("(:goal (and\n")
 
-        # All drones should end up at the depot
+        # All Drones should end up at the depot
         for x in drone:
+            f.write("\n")
+            # TODO: Write a goal that the drone x is at the depot
             f.write("\t(at-drone " + x + " depot)\n")
 
-        # Each person that needs a certain content type should have it
         for x in range(options.persons):
             for y in range(len(content_types)):
                 if need[x][y]:
                     person_name = person[x]
                     content_name = content_types[y]
+                    # TODO: write a goal that the person needs a crate
+                    # with this specific content
                     f.write("\t(person-has-content " + person_name + " " + content_name + ")\n")
 
         f.write("\t))\n")
